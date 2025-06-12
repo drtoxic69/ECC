a7d8f9972ad8a6b97c8cd295dcacab1794275741
Shivakumar <shivakumarjagadish12@gmail.com>
2025-06-12 23:34:40 +0530

    add test for subtraction and multiplication

--- tests/test_field.py
+++ tests/test_field.py
@@ -12,3 +12,15 @@ def test_exponentiation():
     assert a ** 12 == FieldElement(1, 13)
 
 
+def test_multiplication():
+    a = FieldElement(7, 13)
+    b = FieldElement(12, 13)
+    assert a * b == FieldElement(6, 13)
+
+
+def test_subtraction():
+    a = FieldElement(7, 13)
+    b = FieldElement(12, 13)
+    assert b - a == FieldElement(5, 13)
+
+
