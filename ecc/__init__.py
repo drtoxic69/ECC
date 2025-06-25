60bec34711643da8d6b741a3e2a44cd95db76618
Shivakumar <shivakumarjagadish12@gmail.com>
2025-06-25 23:50:16 +0530

    make curves/ folder to test the curves, add secp256k1
    using python files to import the values of curves.

--- ecc/__init__.py
+++ ecc/__init__.py
@@ -1,2 +1,5 @@
 from .field import FieldElement
+from .point import Point
+from .curve import Curve
 
+from .curves.secp256k1 import secp256k1, G as secp256k1_G
---
+++ ecc/curves/__init__.py
@@ -0,0 +1,1 @@
+from .secp256k1 import secp256k1
